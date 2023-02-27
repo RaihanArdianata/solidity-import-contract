f96f654fc0b187c3c9a4d619c310e4dbaff691da
raihan ardianata <raihan.ardianata@gmail.com>
2023-02-27 16:33:16 +0700

    change to array and push new contract to array

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.0; //^ meaning any version of 0.8 will work
 import './SimpleStorage.sol';
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public{
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
 }
