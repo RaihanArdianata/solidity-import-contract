9b873378b5c44d0425af6b2d2f3f69d2ce54edb5
raihan ardianata <raihan.ardianata@gmail.com>
2023-02-27 17:47:18 +0700

    add function to store and get data

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -8,6 +8,18 @@ contract StorageFactory {
 
     function createSimpleStorageContract() public{
         SimpleStorage simpleStorage = new SimpleStorage();
-        simpleStorageArray.push(simpleStorage);
+        simpleStorageArray.push(simpleStorage); 
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address
+        // ABI - Application Binary Interface
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex]; // return simpleStorageArray[_simpleStorageIndex].retrieve()
+        return simpleStorage.retrieve();
     }
 }
