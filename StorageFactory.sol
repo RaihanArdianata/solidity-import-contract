15d8829a51954361d7b640b938349baabb208959
raihan ardianata <raihan.ardianata@gmail.com>
2023-02-27 16:24:57 +0700

    add import SimpleStorage.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0; //^ meaning any version of 0.8 will work 
 
+import './SimpleStorage.sol';
+
 contract StorageFactory {
     SimpleStorage public simpleStorage;
 
